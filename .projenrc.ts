31fc89958292b4bd26fe6475bd78598351eb8615
Matt Ruiz <MattRuiz@eaton.com>
2025-02-07 14:17:16 -0500

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { cdktf } from 'projen';
+const project = new cdktf.ConstructLibraryCdktf({
+  author: 'Matt Ruiz',
+  authorAddress: 'MattRuiz@eaton.com',
+  cdktfVersion: '^0.13.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.7.0',
+  name: 'projen_demo_cdktf',
+  projenrcTs: true,
+  repositoryUrl: 'git@github.com-mesh:mattruizmesh/projen_demo_cdktf.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
